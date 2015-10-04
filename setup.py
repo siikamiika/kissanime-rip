7c4c19a85973e2714a6e87b42e59243aae2076b6
siikamiika <siikamiika@users.noreply.github.com>
2015-10-05 01:48:10 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='kissanime_rip',
+      version='1.0',
+      description='A command line tool for saving KissAnime\'s videos for use with better video players',
+      author='siikamiika',
+      url='https://github.com/siikamiika/kissanime-rip',
+      scripts=['kissanime-rip.py'],
+     )
